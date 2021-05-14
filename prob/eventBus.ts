5ce246f8256622711bc10f33e65a6ee1b00005fb
EurekaHaha <wangzhaosafe@163.com>
2021-05-14 22:16:53 +0800

    5.14

--- prob/eventBus.ts
+++ prob/eventBus.ts
@@ -1,5 +1,4 @@
 type EventName = string | Symbol;
-type useMapName = 'eventsNormal' | 'eventsOnce';
 
 class MyEvent {
 
@@ -11,43 +10,45 @@ class MyEvent {
         this.eventsOnce = new Map();
     }
 
-    addEvent(eventName: EventName, isOnce: boolean, fn: Function, ...args): void {
-        const useEventMap = this.getUseEventMapName(isOnce);
-        this[useEventMap].set(eventName, (...newArgs) => {
+    private bindEvent(eventName: EventName, isOnce: boolean, fn: Function, ...args): void {
+        const useEventMap = this.getUseEventMap(isOnce);
+        useEventMap.set(eventName, (...newArgs) => {
             return fn(...args, ...newArgs);
         })
     }
 
-    on(eventName: EventName, fn: Function, ...args): void {
-        this.addEvent(eventName, false, fn, ...args);
+    addEvent(eventName: EventName, fn: Function, ...args): void {
+        this.bindEvent(eventName, false, fn, ...args);
     }
 
-    once(eventName: EventName, fn: Function, ...args): void {
-        this.addEvent(eventName, true, fn, ...args);
+    addOneTimeEvent(eventName: EventName, fn: Function, ...args): void {
+        this.bindEvent(eventName, true, fn, ...args);
     }
 
-    fire(eventName: EventName, ...args) {
+    execute(eventName: EventName, ...args) {
         if (this.eventsNormal.has(eventName)) {
             this.eventsNormal.get(eventName)(...args);
         } else if (this.eventsOnce.has(eventName)) {
             this.eventsOnce.get(eventName)(...args);
             this.eventsOnce.delete(eventName);
         } else {
-            throw new Error('无此事件名称');
+            console.error('无此事件名称');
+            // throw new Error('无此事件名称');
         }
     }
 
     remove(eventName: EventName, isOnce: boolean = false): void {
-        const useEventMap = this.getUseEventMapName(isOnce);
-        if (this[useEventMap].has(eventName)) {
-            this[useEventMap].delete(eventName);
+        const useEventMap = this.getUseEventMap(isOnce);
+        if (useEventMap.has(eventName)) {
+            useEventMap.delete(eventName);
         } else {
-            throw new Error('无次事件名称');
+            console.error('无次事件名称');
+            // throw new Error('无次事件名称');
         }
     }
 
-    private getUseEventMapName(isOnce: boolean): useMapName {
-        return isOnce ? 'eventsOnce' : 'eventsNormal';
+    private getUseEventMap(isOnce: boolean): Map<EventName, Function> {
+        return isOnce ? this.eventsOnce : this.eventsNormal;
     }
 }
 
@@ -55,9 +56,9 @@ const myEvent = new MyEvent();
 const fn1 = (...args) => {
     console.log('测试', ...args)
 }
-myEvent.on('sleep', fn1, 1, 2, 3);
-myEvent.fire('sleep', 4, 5, 6);
+myEvent.addEvent('sleep', fn1, 1, 2, 3);
+myEvent.execute('sleep', 4, 5, 6);
 myEvent.remove('sleep');
-myEvent.once('sleep1', fn1, 1, 2);
-myEvent.fire('sleep1');
+myEvent.addOneTimeEvent('sleep1', fn1, 1, 2);
+myEvent.execute('sleep1');
 console.log(myEvent.eventsOnce)
